fe214b5aeedab95be65c6a00d734b5c557b84c5c
Alexey Aristov <aav@suprematic.de>
2020-09-14 23:52:51 +0200

    initial

---
+++ README.md
@@ -0,0 +1,93 @@
+<h1 align="center">async-metronome</h1>
+<div align="center">
+ <strong>
+   Unit testing framework for async Rust
+ </strong>
+</div>
+
+<br/>
+
+This crate implements a async unit testing framework, which is based on the  **MutithreadedTC** developed by William Pugh and Nathaniel Ayewah at the University of Maryland.
+
+> *"MultithreadedTC is a framework for testing concurrent applications. It features a 
+> metronome that is used to provide fine control over the sequence of activities in
+> multiple threads." [...](https://www.cs.umd.edu/projects/PL/multithreadedtc/overview.html)*
+
+Concurrent applications are often not deterministic, and so are failures. Depending on the interleavings of threads, they may appear and disappear.
+
+With **async-metronome**, it is possible to define test cases that demonstrate a fixed interleaving of threads. If concurrent abstractions under test are reasonably small, it should be feasible to create multiple test cases that will deterministically cover all relevant interleavings.
+
+The current implementation is based on [async-std](https://crates.io/crates/async-std), but in the future, it should be possible to support [tokio](https://tokio.rs) as well.
+
+To avoid copying original documentation here, please check [MultithreadedTC](https://www.cs.umd.edu/projects/PL/multithreadedtc/) web site, and the corresponding [paper](https://www.cs.umd.edu/projects/PL/multithreadedtc/pubs/ASE07-pugh.pdf).
+
+## Example
+
+```rust
+use async_metronome::{assert_tick, await_tick};
+
+async fn test_send_receive() {
+    let (mut sender, mut receiver) = mpsc::channel::<usize>(1);
+
+    let sender = async move {
+        sender.send(42).await;
+        sender.send(17).await;
+        assert_tick!(1);
+    };
+
+    let receiver = async move {
+        await_tick!(1);
+        receiver.next().await;
+        receiver.next().await;
+    };
+
+    let sender = async_metronome::spawn(sender);
+    let receiver = async_metronome::spawn(receiver);
+
+    sender.await;
+    receiver.await;
+}
+
+#[async_std::test]
+async fn test() {
+    async_metronome::run(test_send_receive()).await;
+}
+```
+## Explanation (adapted from the MutithreadedTC)
+
+async-metronome has an internal clock. The clock only advances to the next tick when all tasks are in a pending state.
+
+The clock starts at `tick 0`. In this example, the macro `await_tick!(1)` makes the receiver block until the clock reaches `tick 1` before resuming. Thread 1 is allowed to run freely in `tick 0`, until it blocks on the call to `sender.send(17)`. At this point, all threads are blocked, and the clock can advance to the next tick.
+
+In `tick 1`, the statement `receiver.next(42)` in the receiver is executed, and this frees up sender. The final statement in sender asserts that the clock is in `tick 1`, in effect asserting that the task blocked on the call to `sender.send(17)`.
+
+Complete example, can be found in the [examples](examples/) directory.
+
+## Installation
+```sh
+$ cargo add async-metronome
+```
+
+<div align="center">
+    <strong style="color: #a00">
+    This is the initial pre-alpha release. Bugs in testing tools are especially dangerous - use with care.
+    </strong>
+</div>
+
+## License
+
+<sup>
+Licensed under either of <a href="LICENSE-APACHE">Apache License, Version
+2.0</a> or <a href="LICENSE-MIT">MIT license</a> at your option.
+</sup>
+
+<br/>
+
+<sub>
+Unless you explicitly state otherwise, any contribution intentionally submitted
+for inclusion in this crate by you, as defined in the Apache-2.0 license, shall
+be dual licensed as above, without any additional terms or conditions.
+</sub>
+ 
+
+
---
+++ src/lib.rs
@@ -0,0 +1,297 @@
+use std::cell::RefCell;
+use std::cmp::Ordering;
+use std::collections::HashMap;
+use std::pin::Pin;
+use std::sync::{Arc, Mutex};
+use std::task::{Context, Poll, Waker};
+use std::time::Duration;
+
+use async_std::task::{self, sleep, TaskId};
+use futures::{future::poll_fn, task::AtomicWaker, Future};
+
+use derive_builder::Builder;
+use pin_project::pin_project;
+
+#[derive(Debug, PartialEq)]
+enum TaskState {
+    Spawned,
+    Running,
+    Pending,
+}
+
+struct TaskEntry {
+    state: TaskState,
+    waker: AtomicWaker,
+}
+
+#[derive(Default, Builder, Debug)]
+pub struct Options {
+    #[builder(setter(into, strip_option))]
+    timeout: Option<Duration>,
+    debug: bool,
+}
+
+struct TestContext {
+    tick: usize,
+    tasks: HashMap<task::TaskId, TaskEntry>,
+    options: Options,
+}
+
+type WrappedTestContext = Arc<Mutex<TestContext>>;
+
+impl TestContext {
+    pub fn new(options: Options) -> Self {
+        Self {
+            tick: 0,
+            tasks: HashMap::new(),
+            options,
+        }
+    }
+
+    fn set_state(&mut self, task_id: &TaskId, state: TaskState) {
+        match self.tasks.get_mut(&task_id) {
+            Some(entry) => {
+                entry.state = state;
+            }
+            None => {
+                panic!();
+            }
+        }
+    }
+
+    fn spawned(&mut self, task_id: TaskId) {
+        log::trace!("{:?} -> Spawned", task_id);
+
+        let entry = TaskEntry {
+            state: TaskState::Spawned,
+            waker: AtomicWaker::new(),
+        };
+        self.tasks.insert(task_id, entry);
+    }
+
+    fn running(&mut self, task_id: &TaskId) {
+        log::trace!("{:?} -> Running", task_id);
+        self.set_state(task_id, TaskState::Running);
+    }
+
+    fn ready(&mut self, task_id: &TaskId) {
+        log::trace!("{:?} -> Ready", task_id);
+        self.tasks.remove(&task_id);
+    }
+
+    // returns true if we are in possible deadlock
+    fn pending(&mut self, task_id: &TaskId) -> bool {
+        log::trace!("{:?} -> Pending", task_id);
+        self.set_state(task_id, TaskState::Pending);
+
+        if self.is_all_pending() {
+            log::trace!("{:?} ** all pending", task_id);
+            if self.has_waiting_for_tick() {
+                log::trace!("{:?} ** tick", task_id);
+                self.next_tick();
+                false
+            } else {
+                log::trace!("{:?} !! deadlock", task_id);
+                true
+            }
+        } else {
+            false
+        }
+    }
+
+    fn is_all_pending(&self) -> bool {
+        self.tasks
+            .values()
+            .all(|entry| entry.state == TaskState::Pending)
+    }
+
+    fn has_waiting_for_tick(&self) -> bool {
+        self.tasks.values().any(|entry| match entry.waker.take() {
+            Some(waker) => {
+                entry.waker.register(&waker);
+                true
+            }
+            None => false,
+        })
+    }
+
+    fn next_tick(&mut self) {
+        self.tick += 1;
+
+        for entry in self.tasks.values() {
+            entry.waker.wake();
+        }
+    }
+
+    fn register_waker(&mut self, task_id: TaskId, waker: &Waker) {
+        self.tasks
+            .get(&task_id)
+            .expect("no task entry")
+            .waker
+            .register(waker);
+    }
+}
+
+thread_local! {
+    static CONTEXT: RefCell<Option<WrappedTestContext>> = RefCell::new(None);
+}
+
+#[doc(hidden)]
+pub fn get_tick() -> usize {
+    CONTEXT.with(|state| state.borrow().as_ref().unwrap().lock().unwrap().tick)
+}
+
+#[macro_export]
+macro_rules! assert_tick {
+    ($expected:expr) => {
+        let actual = async_metronome::get_tick();
+        assert!(
+            actual == $expected,
+            "tick mismatch: expected={}, actual={}",
+            $expected,
+            actual
+        )
+    };
+}
+
+#[doc(hidden)]
+pub fn wait_tick(tick: usize) -> impl Future<Output = usize> {
+    let task_id = task::current().id();
+
+    log::trace!("{:?} ** tick_wait / wait", task_id);
+
+    poll_fn(move |cx| {
+        CONTEXT.with(move |cell| {
+            let cell = cell.borrow();
+            let mut context = cell.as_ref().unwrap().lock().unwrap();
+
+            match context.tick.cmp(&tick) {
+                Ordering::Equal => {
+                    log::trace!("{:?} ** tick_wait / ready", task_id);
+                    Poll::Ready(tick)
+                }
+                Ordering::Less => {
+                    log::trace!("{:?} ** tick_wait / early", task_id);
+                    context.register_waker(task_id, cx.waker());
+
+                    Poll::Pending
+                }
+                Ordering::Greater => {
+                    panic!();
+                }
+            }
+        })
+    })
+}
+
+#[macro_export]
+macro_rules! await_tick {
+    ($tick:expr) => {
+        async_metronome::wait_tick($tick as usize).await
+    };
+}
+
+#[pin_project]
+struct TaskWrapper<T> {
+    context: WrappedTestContext,
+
+    #[pin]
+    task: T,
+}
+
+impl<T: Future> Future for TaskWrapper<T> {
+    type Output = T::Output;
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        let context = self.context.clone();
+        let task_id = task::current().id();
+
+        CONTEXT.with(move |cell| {
+            context.lock().unwrap().running(&task_id);
+
+            cell.replace(Some(context));
+            let poll = self.project().task.poll(cx);
+            let context = cell.replace(None).unwrap();
+
+            let result = if let Poll::Ready(result) = poll {
+                context.lock().unwrap().ready(&task_id);
+                Poll::Ready(result)
+            } else {
+                let mut locked = context.lock().unwrap();
+                let deadlock = locked.pending(&task_id);
+                if deadlock {
+                    match locked.options.timeout {
+                        Some(timeout) => {
+                            let tick = locked.tick;
+                            let context = context.clone();
+                            task::spawn(async move {
+                                sleep(timeout).await;
+                                let current_tick = context.lock().unwrap().tick;
+                                if current_tick == tick {
+                                    panic!("{:?} !! deadlock", task_id)
+                                }
+                            });
+                        }
+                        None => {
+                            drop(locked);
+                            panic!("{:?} !! deadlock", task_id);
+                        }
+                    }
+                }
+
+                Poll::Pending
+            };
+
+            result
+        })
+    }
+}
+
+// instrument task and spawn it. Will panic if executed outside of
+// already intstrumented task.
+pub fn spawn<T, O>(task: T) -> task::JoinHandle<O>
+where
+    O: Send + 'static,
+    T: Future<Output = O> + Send + 'static,
+{
+    CONTEXT.with(move |cell| {
+        let cell = cell.borrow();
+        let context = cell.as_ref().expect("nocontext");
+
+        let mut locked = context.lock().unwrap();
+
+        let wrapped = TaskWrapper {
+            task,
+            context: context.clone(),
+        };
+
+        let handle = task::spawn(wrapped);
+
+        locked.spawned(handle.task().id());
+
+        handle
+    })
+}
+
+// run test case
+pub fn run_opt<T, O>(test: T, options: Options) -> task::JoinHandle<O>
+where
+    O: Send + 'static,
+    T: Future<Output = O> + Send + 'static,
+{
+    CONTEXT.with(move |cell| {
+        let context = Arc::new(Mutex::new(TestContext::new(options)));
+
+        cell.replace(Some(context));
+        spawn(test)
+    })
+}
+
+// run test case with default options
+pub fn run<T, O>(test: T) -> task::JoinHandle<O>
+where
+    O: Send + 'static,
+    T: Future<Output = O> + Send + 'static,
+{
+    run_opt(test, Options::default())
+}
