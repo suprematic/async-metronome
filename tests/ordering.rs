a26f669613c1194dac3957fce72b7428a36a6107
Alexey Aristov <aav@suprematic.de>
2020-11-01 21:29:30 +0100

    add tests/ordering.rs

---
+++ tests/ordering.rs
@@ -0,0 +1,39 @@
+use std::sync::{
+    atomic::{AtomicUsize, Ordering},
+    Arc,
+};
+
+#[async_metronome::test]
+async fn test_ordering() {
+    let ai1 = Arc::new(AtomicUsize::new(0));
+    let ai2 = ai1.clone();
+    let ai3 = ai1.clone();
+
+    let ordering = Ordering::SeqCst;
+
+    let task1 = async move {
+        ai1.compare_and_swap(0, 1, ordering);
+        async_metronome::await_tick!(3);
+        assert_eq!(ai1.load(ordering), 3);
+    };
+
+    let task2 = async move {
+        async_metronome::await_tick!(1);
+        assert_eq!(ai2.compare_and_swap(1, 2, ordering), 1);
+        async_metronome::await_tick!(3);
+        assert_eq!(ai2.load(ordering), 3);
+    };
+
+    let task3 = async move {
+        async_metronome::await_tick!(2);
+        assert_eq!(ai3.compare_and_swap(2, 3, ordering), 2);
+    };
+
+    let s1 = async_metronome::spawn(task1);
+    let s2 = async_metronome::spawn(task2);
+    let s3 = async_metronome::spawn(task3);
+
+    s1.await;
+    s2.await;
+    s3.await;
+}
